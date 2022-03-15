c0733c209309e4b238f82c5a57d2f487ef3c061c
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2022-03-15 16:17:59 +0100

    finished display in articles index

--- components/ArticlesByYear.vue
+++ components/ArticlesByYear.vue
@@ -1,9 +1,9 @@
 <template>
 	<div>
 		<h2>{{ year }}</h2>
-		<v-divider></v-divider>
-		<v-btn block text nuxt v-for="article in articles" :key="article.title" :to="`/articles/${article.slug}`">
-			{{ article.title }} <v-spacer></v-spacer> {{ article.publication_date }}
+		<v-divider class="mb-2"></v-divider>
+		<v-btn class="no-uppercase" block text nuxt v-for="article in articles" :key="article.title" :to="`/articles/${article.slug}`">
+			<span>{{ article.title }}</span> <v-spacer></v-spacer> <span class="text-lowercase grey--text">{{ readableDate(article.publication_date) }}</span>
 		</v-btn>
 	</div>
 </template>
@@ -12,9 +12,17 @@ export default {
 	props: {
 		year: "",
 		articles: []
+	},
+	methods: { 
+		readableDate(date) {
+			date = new Date(date)
+			return date.getDay() + " " + date.toLocaleString('default', { month: 'long' })
+		}
 	}
 }
 </script>
 <style>
-	
+.no-uppercase {
+	text-transform: unset;
+}
 </style>
