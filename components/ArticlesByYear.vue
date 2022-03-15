d8697f5df4c6d158ee174c802bd5265160809e3d
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2022-03-15 15:35:00 +0100

    added new way articles are displayed in index

---
+++ components/ArticlesByYear.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<h2>{{ year }}</h2>
+		<v-divider></v-divider>
+		<v-btn block text nuxt v-for="article in articles" :key="article.title" :to="`/articles/${article.slug}`">
+			{{ article.title }} <v-spacer></v-spacer> {{ article.publication_date }}
+		</v-btn>
+	</div>
+</template>
+<script>
+export default {
+	props: {
+		year: "",
+		articles: []
+	}
+}
+</script>
+<style>
+	
+</style>
--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -5,32 +5,56 @@
 				md="6"
 				offset-md="3"
 			>
-				<h2 align="center">Mes articles</h2>
+				<h2 align="center" class="mt-10">Mes articles</h2>
 				<br>
-				<v-card v-for="article in articles" :key="article.id" :to="`/articles/${article.slug}`" >
-					<v-card-title primary-title>
-						{{ article.title }}
-					</v-card-title>
-				</v-card>
+				<div align="center" class="mb-5">
+					Articles, tutoriels, documentations techniques, revues, rapport, recherches, cours..
+					<br>
+					L'ensemble de ma bibliographie est ici. 
+				</div>
+				<articles-by-year v-for="(item, i) in articles_by_year" :key="i" :year="item.year" :articles="item.articles_list" />
 			</v-col>
 		</v-row>
 	</v-container>
 </template>
 <script>
+import ArticlesByYear from '~/components/ArticlesByYear.vue'
 import Api from "../../services/api/api"
 
+// preparing a function to use api efficiently
 async function getArticlesPromise() {
 	return await Api.getArticles()
 } 
 
 export default {
+	components: { ArticlesByYear },
 	data() {
 		return {
-			articles: []
+			articles: [],
+			articles_by_year: []
 		}
 	},
 	async mounted() {
+		// retrieving data from api
 		this.articles = (await getArticlesPromise()).data
+		// sorting articles just retrieved by year of publication
+		// I use an array with objects inside, each object being a year (string) and a articles_list (array of articles) for that year
+		this.articles.forEach(article => {
+			const article_year = article.publication_date.substring(0,4)
+			// testing if year already in articles_by_year list => pushing the article into the linked list
+			if (this.articles_by_year.some(elem => elem.year === article_year)) {
+				const index = this.articles_by_year.findIndex(elem => elem.year === article_year)
+				this.articles_by_year[index].articles_list.push(article)
+			
+			// else it means year is not in the list so pushing a new year to the list with its linked empty array
+			// I'm using the fact that .push() returns the new length of the list to substract 1 giving me access to the last element of this array (being our new object)
+			// then I can push the article into the new empty articles_list I just created
+			} else {
+				this.articles_by_year[
+					this.articles_by_year.push({year: article_year, articles_list: []})-1
+					].articles_list.push(article)
+			}
+		})
 	},
 }
 </script>
